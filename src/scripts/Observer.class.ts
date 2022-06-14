84da63aa49f36349042830f502c8e6788a5722cd
Bigismall <rondelious@gmail.com>
2022-06-14 12:33:50 +0200

    Updates

--- src/scripts/Observer.class.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "./Message.type";
-
-export default abstract class Observer {
-  protected constructor() {}
-
-  update(publication: Message) {
-    console.log(`Publication [Observer] ${publication}`);
-  }
-}
