f0dcc08830d60799ed9567031031e5d20802c200
Slawomir Rodak <Bigismall@users.noreply.github.com>
2025-09-22 07:31:26 +0200

    fix: replace biome plugin to work with biome 2.x (#96)

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import biomePlugin from "vite-plugin-biome";
+import { defineConfig } from 'vite';
+import { biomePlugin } from '@pbr1111/vite-plugin-biome';
 
-export default {
-	plugins: [
-		biomePlugin({
-			mode: "check",
-			files: "./src/",
-			applyFixes: true,
-		}),
-	],
-	base: "/",
-};
+export default defineConfig({
+    plugins: [biomePlugin({
+        mode: "check",
+        files: "./src/",
+        applyFixes: true,
+    })],
+    base: "/",
+});
